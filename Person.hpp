cc61d7aedec5c86fb25d365f6f2bfa00be5b7851
Tomasz K <94.sham@gmail.com>
2018-07-31 18:27:54 +0200

    Stage 2 Person.hpp

--- Person.hpp
+++ Person.hpp
@@ -3,9 +3,10 @@
 
 class Person {
 protected:
+	static const char alias_;
 	std::string name_;
 	std::string surname_;
-	std::string PESEL_;
+	std::string Pesel_;
 	std::string gender_;
 	std::string address_;
 public:
@@ -13,13 +14,15 @@ public:
 	
 	std::string getName() const { return name_; }
 	std::string getSurname() const { return surname_; }
-	std::string getPESEL() const { return PESEL_; }
+	std::string getPesel() const { return Pesel_; }
 	std::string getGender() const { return gender_; }
 	std::string getAddress() const { return address_; }
+	
 	void setAddress(std::string address) { address_ = address;}
-
+	
 	//temporary solution of bad project concept
 	virtual int getIndex() = 0;
 	virtual float getSalary() = 0;
 	virtual void setSalary(float) = 0;
+	virtual char getAlias() = 0;
 };
