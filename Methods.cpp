bf4ec22be8b0f3686d3a2b96c64b533fe76fd73f
Tomasz K <94.sham@gmail.com>
2018-07-31 18:27:10 +0200

    Stage 2 Methods.cpp

--- Methods.cpp
+++ Methods.cpp
@@ -7,22 +7,22 @@
 /* PERSON CLASS */
 
 void Person::show() {
-	std::cout << name_ << " " << surname_ << ", " << PESEL_ <<
+	std::cout << name_ << " " << surname_ << ", " << Pesel_ <<
 		", " << gender_ << ", " << address_ << "\n";
 }
 
 /* STUDENT CLASS */
 
 void Student::show() {
-	std::cout <<"["<< Student::alias_ << "] " << name_ << " " << surname_ << ", " << PESEL_ <<
+	std::cout <<"["<< Student::alias_ << "] " << name_ << " " << surname_ << ", " << Pesel_ <<
 		", " << gender_ << ", " << address_ <<", " << index_ << "\n";
 }
 
 	Student::Student(std::string name, std::string surname,
-	std::string PESEL, std::string gender, std::string address, int index) {
+	std::string Pesel, std::string gender, int index, std::string address) {
 	name_ = name;
 	surname_ = surname;
-	PESEL_ = PESEL;
+	Pesel_ = Pesel;
 	gender_ = gender;
 	address_ = address;
 	index_ = index;
@@ -32,15 +32,15 @@ void Student::show() {
 /* EMPLOYEE CLASS */
 
 void Employee::show() {
-	std::cout << "[" << Employee::alias_ << "] " << name_ << " " << surname_ << ", " << PESEL_ <<
+	std::cout << "[" << Employee::alias_ << "] " << name_ << " " << surname_ << ", " << Pesel_ <<
 		", " << gender_ << ", " << address_ << ", " << salary_ << "$\n";
 }
 
 	Employee::Employee(std::string name, std::string surname,
-	std::string PESEL, std::string gender, std::string address, float earnings){
+	std::string Pesel, std::string gender,  float earnings, std::string address){
 	name_ = name;
 	surname_ = surname;
-	PESEL_ = PESEL;
+	Pesel_ = Pesel;
 	gender_ = gender;
 	address_ = address;
 	salary_ = earnings;
